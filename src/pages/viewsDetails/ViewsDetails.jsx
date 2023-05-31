ec282cf8c98f6ab32f4c1034a139ec69170e69c5
Landry SAWA INGABIRE <myportbusiness022@gmail.com>
2023-05-31 19:53:46 +0300

    kaya3

--- src/pages/viewsDetails/ViewsDetails.jsx
+++ src/pages/viewsDetails/ViewsDetails.jsx
@@ -8,12 +8,12 @@ const ViewsDetails = () => {
         <div className="container-fluid page-header mb-5 p-0 bg-dark" style={{ backgroundImage: "url(../../assets/image/notes/mth18.jpg)" }}>
             <div className="container-fluid page-fluid-inner py-5">
                 <div className="container text-center pb-5">
-                <h1 className="display-3 text-white mb-3 aniamted slideInDown">Booking</h1>
+                <h1 className="display-3 text-white mb-3 aniamted slideInDown">Is coming soon...</h1>
                 <nav aria-label='breadcrumb'>
                     <ol className="breadcrumb justify-content-center text-uppercase">
                     <li className="breadcrumb-item"><Link href="/" className='text-decoration-none'>Home</Link> </li>
                     <li className="breadcrumb-item"><Link to="/rooms" className='text-decoration-none'>Rooms</Link> </li>
-                    <li className="breadcrumb-item text-white active">Booking</li>
+                    <li className="breadcrumb-item text-white active">Coming</li>
                     </ol>
                 </nav>
                 </div>
